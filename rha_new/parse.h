84992421ef2a8958c713050ec56f0d5dffafcf7b
Stefan Harmeling <harmeling@users.noreply.github.com>
2007-08-08 22:49:49 +0000

    moved _rha_ in parse.h

--- rha_new/parse.h
+++ rha_new/parse.h
@@ -3,15 +3,15 @@
 
 #include "rha_types.h"
 
-extern object_t _rha_ parse(string_t);
+extern _rha_ object_t parse(string_t);
 
-extern object_t _rha_ add_keyword(string_t, int_t);
+extern _rha_ object_t add_keyword(string_t, int_t);
 
-extern object_t _rha_ add_macro_prule(string_t, object_t);
-extern object_t _rha_ add_postfix_prule(string_t, object_t);
-extern object_t _rha_ add_prefix_prule(string_t, object_t);
-extern object_t _rha_ add_infix_prule(string_t, object_t, double_t prec, int_t bind);
-extern object_t _rha_ add_freefix_prule(string_t, object_t, tuple_t parts);
+extern _rha_ object_t add_macro_prule(string_t, object_t);
+extern _rha_ object_t add_postfix_prule(string_t, object_t);
+extern _rha_ object_t add_prefix_prule(string_t, object_t);
+extern _rha_ object_t add_infix_prule(string_t, object_t, double_t prec, int_t bind);
+extern _rha_ object_t add_freefix_prule(string_t, object_t, tuple_t parts);
 
 #define LEFT_BIND -1
 #define NO_BIND    0
