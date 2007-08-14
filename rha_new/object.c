69a1c39dd4543842247b51d409dbaf69a3165cec
Stefan Harmeling <harmeling@users.noreply.github.com>
2007-08-14 18:47:31 +0000

    put back "ls" in object_init(), later it should go in prelude.rha

--- rha_new/object.c
+++ rha_new/object.c
@@ -32,6 +32,7 @@ void object_init(object_t root, object_t module)
   // the only two function we need to pull by hand (and not in prelude.rha)
   assign(root, lookup_sym, lookup(module, lookup_sym));
   assign(root, assign_sym, lookup(module, assign_sym));
+  assign(root, ls_sym, lookup(module, ls_sym));
 }
 
 
